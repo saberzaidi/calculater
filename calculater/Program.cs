68e660a4a99684aa83f769d8a7e1360b957d650b
saber zaidi <sabir_xaidi@yahoo.com>
2020-09-14 19:02:18 +0200

    Add project files.

---
+++ calculater/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my calculater");
+            double a;
+            double b;
+            double c;
+            string options;
+            Console.WriteLine("Please Enter First no");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please select options:(+,-,*,/");
+            options = Console.ReadLine();
+            Console.WriteLine("Please Enter Second no");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (options == "+")
+
+            {
+                c = a + b;
+                Console.WriteLine(c);
+            }
+
+            if (options == "-")
+
+            {
+                c = a - b;
+                Console.WriteLine(c);
+            }
+            if (options == "*")
+
+            {
+                c = a * b;
+                Console.WriteLine(c);
+            }
+            if (options == "/")
+
+            {
+                c = a / b;
+                Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
